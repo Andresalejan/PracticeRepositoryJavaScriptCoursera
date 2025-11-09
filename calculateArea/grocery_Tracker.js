cf050c74922ab67a8cc98af43666cb661d653a3a
Andresalejan <Andresalejan3@gmail.com>
2025-11-09 11:41:38 -0500

    commit grocerty tracker exercise

---
+++ calculateArea/grocery_Tracker.js
@@ -0,0 +1,12 @@
+let grocery1, grocery2, grocery3;
+
+function groceryTracker() {
+    //Get value of each grocery item
+    grocery1 = parseFloat(document.getElementById('grocery1').value);
+    grocery2 = parseFloat(document.getElementById('grocery2').value);
+    grocery3 = parseFloat(document.getElementById('grocery3').value);
+    //Calculate area
+    let groceryTotal = grocery1 + grocery2 + grocery3;
+
+    document.getElementById('result').innerText = `The total amount is: ${groceryTotal}`;
+}
